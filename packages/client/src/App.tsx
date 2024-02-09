d0a1611374651b7ab2a2c99675fbc1ddde248941
gonza <alvarezgonzalo0022@gmail.com>
2024-02-09 18:56:20 -0300

    Adding code splitting

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import HomePage from "./pages/HomePage.tsx";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -7,43 +6,44 @@ import {
   Navigate,
 } from "react-router-dom";
 import { UserProvider } from "./context/userContext.tsx";
-import ErrorPage from "./pages/static/ErrorPage.tsx";
-import Header from "./components/header/Header.tsx";
-import Footer from "./components/footer/SimpleFiveColumn.tsx";
-import LoginPage from "./pages/static/LoginPage.tsx";
-import PrivacyPolicy from "./pages/static/GDPR/PrivacyPolicy.tsx";
-import TermsOfService from "./pages/static/GDPR/TermsOfService.tsx";
-import SignUpPage from "./pages/static/SignUpPage.tsx";
-import Cart from "./pages/Cart.tsx";
 
+const HomePage = React.lazy(() => import("./pages/HomePage.tsx"));
+const ErrorPage = React.lazy(() => import("./pages/static/ErrorPage.tsx"));
+const Header = React.lazy(() => import("./components/header/Header.tsx"));
+const Footer = React.lazy(() => import("./components/footer/SimpleFiveColumn.tsx"));
+const LoginPage = React.lazy(() => import("./pages/static/LoginPage.tsx"));
+const PrivacyPolicy = React.lazy(() => import("./pages/static/GDPR/PrivacyPolicy.tsx"));
+const TermsOfService = React.lazy(() => import("./pages/static/GDPR/TermsOfService.tsx"));
+const SignUpPage = React.lazy(() => import("./pages/static/SignUpPage.tsx"));
+const Cart = React.lazy(() => import("./pages/Cart.tsx"));
 
 function App() {
   return (
-    <>
-      <UserProvider>
-        <Router>
+    <UserProvider>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
           <Header />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/stanleystella" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/cart" element={<Cart/>} />
+            <Route path="/cart" element={<Cart />} />
             <Route
               path="/privacypolicy"
               element={<PrivacyPolicy headingText="Privacy Policy" />}
             />
             <Route
-              path="termsofservice"
+              path="/termsofservice"
               element={<TermsOfService headingText="Terms Of Service" />}
             />
             <Route path="/404" element={<ErrorPage />} />
             <Route path="*" element={<Navigate to="/404" />} />
           </Routes>
           <Footer />
-        </Router>
-      </UserProvider>
-    </>
+        </Suspense>
+      </Router>
+    </UserProvider>
   );
 }
 
