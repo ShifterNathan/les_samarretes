c09e1e7243da6e9da1ccb5397d116811c8f486e2
gonzalo-alvarezalvarez_cencosud <gonzalo-alvarezalvarez_cencosud>
2024-02-19 21:54:37 -0300

    Continuing

--- packages/client/src/components/header/DesktopMenu.tsx
+++ packages/client/src/components/header/DesktopMenu.tsx
@@ -19,7 +19,7 @@ const DesktopMenu = (props: IDesktopMenuProps) => {
     wishlist: false,
   });
 
-  const { products, callsToAction, company } = props;
+  const { products, company } = props;
 
   const toggleSlideover = (type: SlideoverType) => {
     setIsSlideoverOpen({ ...isSlideoverOpen, [type]: !isSlideoverOpen[type] });
@@ -27,73 +27,18 @@ const DesktopMenu = (props: IDesktopMenuProps) => {
   return (
     <>
       <Popover.Group className="hidden lg:flex lg:gap-x-12">
-        <Popover className="relative">
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-              <div className="p-4">
-                {products.map((item) => (
-                  <div
-                    key={item.name}
-                    className="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                  >
-                    <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                      <item.icon
-                        className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="flex-auto">
-                      <a
-                        href={item.href}
-                        className="block font-semibold text-gray-900"
-                      >
-                        {item.name}
-                        <span className="absolute inset-0" />
-                      </a>
-                      <p className="mt-1 text-gray-600">{item.description}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                {callsToAction.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                  >
-                    <item.icon
-                      className="h-5 w-5 flex-none text-gray-400"
-                      aria-hidden="true"
-                    />
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </Popover>
-
-        <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+        <a href="/" className="text-md font-semibold leading-6 text-gray-900 hover:text-primary-500">
           Home
         </a>
         <a
           href="/shop"
-          className="text-sm font-semibold leading-6 text-gray-900"
+          className="text-md font-semibold leading-6 text-gray-900 hover:text-primary-500"
         >
           Shop
         </a>
 
         <Popover>
-          <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+          <Popover.Button className="flex items-center gap-x-1 text-md font-semibold leading-6 text-gray-900 hover:text-primary-500">
             Marcas
             <ChevronDownIcon
               className="h-5 w-5 flex-none text-gray-400"
